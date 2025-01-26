7561e8f8825dca49097b57d902102c4c0d79a134
Bridger Tower <bridgertower@gmail.com>
2025-01-25 22:41:15 -0700

    feat: add metadata to layout.tsx

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,9 +1,35 @@
 import '@/globals.css'
 
+import { Metadata } from 'next'
+import { Geist as FontSans } from 'next/font/google'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Payload SaaS Starter by Bridger Tower',
+  description: 'A modern SaaS starter built with Payload CMS, Next.js, and TypeScript',
+  metadataBase: new URL('https://payload-saas-starter.vercel.app/'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://payload-saas-starter.vercel.app/',
+    siteName: 'Payload SaaS Starter by Bridger Tower',
+    title: 'Payload SaaS Starter by Bridger Tower',
+    description: 'A modern SaaS starter built with Payload CMS, Next.js, and TypeScript',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Payload SaaS Starter by Bridger Tower',
+    description: 'A modern SaaS starter built with Payload CMS, Next.js, and TypeScript',
+  },
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={fontSans.className}>
+      <body className={fontSans.className}>{children}</body>
     </html>
   )
 }
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div className="p-12">
+    <div className="p-6">
       <h1 className="text-3xl">Welcome to Payload!</h1>
     </div>
   )
