4026bf0e418b98ddb57e9676fb5aa249b82edd56
Bridger Tower <bridgertower@gmail.com>
2025-01-25 22:18:23 -0700

    set up cf r2 storage for media

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -39,12 +39,12 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      bucket: process.env.R2_BUCKET!,
+      bucket: process.env.R2_BUCKET || '',
       config: {
-        endpoint: process.env.R2_ENDPOINT!,
+        endpoint: process.env.R2_ENDPOINT || '',
         credentials: {
-          accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+          accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
         },
         region: 'auto',
         forcePathStyle: true,
