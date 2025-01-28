501dde94a9c56dfce444559ac70c9c81c66f645d
Bridger Tower <bridgertower@gmail.com>
2025-01-28 14:28:22 -0700

    lots of updates

---
+++ src/app/(frontend)/(logged-out)/page.tsx
@@ -0,0 +1,65 @@
+import { getGlobal } from '@/lib/globals'
+import { RenderBlocks } from '@/components/render-blocks'
+
+import type { Home } from '@/payload-types'
+import { Section, Container, Prose } from '@/components/craft'
+
+export default async function Index() {
+  const global: Home = await getGlobal({ slug: 'home' })
+
+  return (
+    <main>
+      <RenderBlocks blocks={global?.blocks} />
+      <ToDelete />
+    </main>
+  )
+}
+
+const ToDelete = () => {
+  return (
+    <Section>
+      <Container>
+        <Prose>
+          <h2>What&apos;s included?</h2>
+          <ul className="list-disc list-inside space-y-2 mt-4">
+            <li>Open Source SaaS starter for creating applications with Next.js and Payload CMS</li>
+            <li>User / admin authentication</li>
+            <li>Google Auth & Github Auth integration</li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com">
+                Tailwind 4
+              </a>{' '}
+              with{' '}
+              <a target="_blank" rel="noopener noreferrer" href="https://ui.shadcn.com">
+                shadcn/ui
+              </a>{' '}
+              components
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://craft-ds.com">
+                brijr/craft
+              </a>{' '}
+              design system
+            </li>
+            <li>User Dashboard</li>
+            <li>Editable Homepage using Globals</li>
+            <li>Customizable content blocks</li>
+            <li>Blog functionality</li>
+          </ul>
+          <hr />
+          <p>
+            Created by{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://bridger.to">
+              Bridger Tower
+            </a>
+            . Follow on{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://bridger.to/x">
+              X for updates
+            </a>
+            .
+          </p>
+        </Prose>
+      </Container>
+    </Section>
+  )
+}
--- src/blocks/HeroBlock.tsx
+++ src/blocks/HeroBlock.tsx
@@ -1,7 +1,11 @@
 import { Section, Container, Craft } from '@/components/craft'
 import { Button } from '@/components/ui/button'
+
 import type { Block } from 'payload'
-import type { HeroBlock as HeroBlockProps } from '@/payload-types'
+import type { HeroBlock as HeroBlockProps, Media } from '@/payload-types'
+
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const HeroConfig: Block = {
   slug: 'hero',
@@ -43,29 +47,35 @@ export const HeroConfig: Block = {
 }
 
 export const HeroBlock = (props: HeroBlockProps) => {
+  const image = props.image as Media
+
   return (
     <Section>
       <Container className="space-y-6 md:space-y-12">
-        <Button variant="outline" size="sm">
-          <a href={props.button.link}>{props.button.text}</a>
-        </Button>
         <Craft className="space-y-3">
           <h1>{props.title}</h1>
           <h3 className="text-muted-foreground">{props.subtitle}</h3>
         </Craft>
-        <div className="relative h-[480px] w-full overflow-hidden rounded-lg border">
-          <img
-            // @ts-ignore
-            src={props.image.url}
-            // @ts-ignore
-            width={props.image.width}
-            // @ts-ignore
-            height={props.image.height}
-            // @ts-ignore
-            alt={props.image.alt}
-            className="absolute inset-0 w-full h-full object-cover object-center"
-          />
+
+        <div className="space-x-2">
+          <Button asChild size="sm">
+            <Link href={props.button.link}>{props.button.text}</Link>
+          </Button>
+          <Button asChild variant="outline" size="sm">
+            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbrijr%2Fpayload-saas-starter&env=DATABASE_URI,PAYLOAD_SECRET,R2_ACCESS_KEY_ID,R2_BUCKET,R2_ENDPOINT&project-name=payload-saas-starter&repository-name=payload-saas-starter&demo-title=Payload%20SaaS%20Starter&demo-description=Open%20Source%20SaaS%20starter%20for%20creating%20applications%20with%20Next.js%20and%20Payload&demo-url=https%3A%2F%2Fpayload-saas-starter.vercel.app">
+              Deploy on Vercel
+            </a>
+          </Button>
         </div>
+
+        <Image
+          src={image.url || ''}
+          width={image.width || 0}
+          height={image.height || 0}
+          alt={image.alt || ''}
+          loading="eager"
+          className="rounded-lg border"
+        />
       </Container>
     </Section>
   )
