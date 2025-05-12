3d83d7d3ea7bc577fb22b7c98d5c46ad1e65555e
Bridger Tower <bridgertower@gmail.com>
2025-05-12 09:10:37 -0600

    wip

--- src/app/(frontend)/(site)/page.tsx
+++ src/app/(frontend)/(site)/page.tsx
@@ -14,32 +14,7 @@ const ToDelete = () => {
     <Section>
       <Container>
         <Prose>
-          <h2>What&apos;s included?</h2>
-          <ul className="list-disc list-inside space-y-2 mt-4">
-            <li>Open Source SaaS starter for creating applications with Next.js and Payload CMS</li>
-            <li>User / admin authentication</li>
-            <li>Google Auth & Github Auth integration</li>
-            <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com">
-                Tailwind 4
-              </a>{' '}
-              with{' '}
-              <a target="_blank" rel="noopener noreferrer" href="https://ui.shadcn.com">
-                shadcn/ui
-              </a>{' '}
-              components
-            </li>
-            <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://craft-ds.com">
-                brijr/craft
-              </a>{' '}
-              design system
-            </li>
-            <li>User Dashboard</li>
-            <li>Editable Homepage using Globals</li>
-            <li>Customizable content blocks</li>
-            <li>Blog functionality</li>
-          </ul>
+          <h2>What if Payload CMS could power your next SaaS?</h2>
           <hr />
           <p>
             Created by{' '}
