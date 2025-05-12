8df7c04d12157e3070dc1570c41f289dbb6b1ac0
Bridger Tower <bridgertower@gmail.com>
2025-05-12 09:24:52 -0600

    wip

--- src/app/(frontend)/(admin)/dashboard/page.tsx
+++ src/app/(frontend)/(admin)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { Section, Container } from '@/components/ds'
+import { Section, Container, Prose } from '@/components/ds'
 import { getUser } from '@/lib/auth'
 import { LogoutButton } from '@/components/auth/logout-button'
 
@@ -9,10 +9,21 @@ export default async function Admin() {
 
   return (
     <Section>
-      <Container className="grid gap-6">
-        <h1>Welcome, {user?.email}</h1>
+      <Container className="grid gap-6 grid-cols-[1fr_auto]">
+        <ToDelete user={user} />
         <LogoutButton />
       </Container>
     </Section>
+  )
+}
+
+const ToDelete = ({ user }: { user: User | null }) => {
+  return (
+    <Prose isSpaced>
+      <h3>Welcome, {user?.email}</h3>
+      <p className="text-muted-foreground">
+        This is the admin dashboard. You cannot access this page if you are not logged in.
+      </p>
+    </Prose>
   )
 }
