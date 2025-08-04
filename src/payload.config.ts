add0029af509a39c1a742e806987b005969242a9
Bridger Tower <bridgertower@gmail.com>
2025-08-04 15:14:49 -0600

    update config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,11 +5,8 @@ import { fileURLToPath } from 'url'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 
-// If you want to use Cloudflare R2 or AWS S3, uncomment the following lines
-// import { s3Storage } from '@payloadcms/storage-s3'
-
 import sharp from 'sharp'
-import path from 'path'
+import path from 'node:path'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -45,21 +42,5 @@ export default buildConfig({
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
-
-    // s3Storage({
-    //   collections: {
-    //     media: true,
-    //   },
-    //   bucket: process.env.R2_BUCKET || '',
-    //   config: {
-    //     endpoint: process.env.R2_ENDPOINT || '',
-    //     credentials: {
-    //       accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
-    //       secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
-    //     },
-    //     region: 'auto',
-    //     forcePathStyle: true,
-    //   },
-    // }),
   ],
 })
