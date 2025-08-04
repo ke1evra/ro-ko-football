5989b8f6b0428f7dae3d8231733919a4a9458b26
Bridger Tower <bridgertower@gmail.com>
2025-08-04 14:10:39 -0600

    update footer

--- src/components/site/footer.tsx
+++ src/components/site/footer.tsx
@@ -6,19 +6,17 @@ import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <footer className="border-t bg-accent/30">
+    <footer className="border-t bg-accent/30 text-sm">
       <Section>
         <Container className="grid gap-6 grid-cols-[1fr_auto]">
           <div className="grid gap-6">
-            <Link href="/" className="space-y-6 text-lg">
-              <h3>Payload SaaS Starter</h3>
-            </Link>
             <p className="text-muted-foreground">
               <Balancer>
-                Open Source SaaS starter for creating applications with Next.js and Payload CMS.
+                Payload Starter is an Open Source SaaS starter for creating applications with
+                Next.js and Payload.
               </Balancer>
             </p>
-            <div className="mb-6 flex flex-col gap-4 text-sm text-muted-foreground underline underline-offset-4 md:mb-0 md:flex-row">
+            <div className="mb-6 flex flex-col gap-4 text-xs text-muted-foreground underline underline-offset-4 md:mb-0 md:flex-row">
               <Link href="/privacy-policy">Privacy Policy</Link>
               <Link href="/terms-of-service">Terms of Service</Link>
               <Link href="/cookie-policy">Cookie Policy</Link>
