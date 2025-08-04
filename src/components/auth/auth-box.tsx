47940c543737e4f57ff2651b5cb37b378e73bb1c
Bridger Tower <bridgertower@gmail.com>
2025-08-04 15:04:55 -0600

    update login

--- src/components/auth/auth-box.tsx
+++ src/components/auth/auth-box.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import { ArrowLeft } from 'lucide-react'
 
 export const AuthBox = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full sm:w-sm mx-auto space-y-4">
       <div className="p-6 border rounded-md bg-background">{children}</div>
-      <Link className="text-xs text-muted-foreground" href="/">
-        &larr; Back to home
+      <Link className="text-xs text-muted-foreground flex items-center gap-1" href="/">
+        <ArrowLeft size="12" /> Back to home
       </Link>
     </div>
   )
