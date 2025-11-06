f97e81e2264c8086f73f13dd2d99187d0be3ff17
Konstantin Dolgov <dolgov.konstantin@gmail.com>
2025-11-06 16:31:19 +0300

    feat: add root layout with metadata base URL configuration for RocoScore

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://rocoscore.ru/'),
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
